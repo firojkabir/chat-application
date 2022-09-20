b2e17ce84f8e9f07da82ba6dc7abc81e03f0b8c7
Md Firoj Kabir <firoj.kabir65@gmail.com>
2022-09-20 16:06:47 +0200

    Setup common error

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -6,4 +6,24 @@ function notFoundhandler(req, res, next) {
 }
 
 // default error handler
-function errorHandler(err, req, res, next) {}
+function errorHandler(err, req, res, next) {
+  res.locals.error =
+    process.env.NODE_ENV === "development" ? err : { message: err.message };
+
+  res.status(err.status || 500);
+
+  if (res.locals.html) {
+    // html response
+    res.render("error", {
+      title: "Error page",
+    });
+  } else {
+    // json response
+    res.json(res.locals.error);
+  }
+}
+
+module.exports = {
+  notFoundhandler,
+  errorHandler,
+};
