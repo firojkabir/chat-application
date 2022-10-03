983d7d3b39e151ddbbead9410e582c00600d8e78
Md Firoj Kabir <firoj.kabir65@gmail.com>
2022-10-03 15:21:03 +0200

    Login functionality using cookie auth

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,7 +1,74 @@
+// external imports
+const bcrypt = require("bcrypt");
+const createError = require("http-errors");
+const jwt = require("jsonwebtoken");
+
+// internal imports
+const User = require("../models/People");
+
+// get login page
 function getLogin(req, res, next) {
   res.render("index");
 }
 
+// do login
+async function login(req, res, next) {
+  try {
+    // find a user who has email/username
+    const user = await User.findOne({
+      $or: [{ email: req.body.username }, { mobile: req.body.username }],
+    });
+
+    if (user && user._id) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+
+      if (isValidPassword) {
+        const userObject = {
+          username: user.modelName,
+          mobile: user.mobile,
+          email: user.email,
+          role: "user",
+        };
+
+        //generate token
+        const token = jwt.sign(userObject, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRY,
+        });
+
+        // set cookie
+        res.cookie(process.env.COOKIE_NAME, token, {
+          maxAge: process.env.JWT_EXPIRY,
+          httpOnly: true,
+          signed: true,
+        });
+
+        // set logged in user local identification
+        res.locals.loggedInUser = userObject;
+        res.render("inbox");
+      } else {
+        throw createError("Login failed! Please try agian.");
+      }
+    } else {
+      throw createError("Login failed! Please try agian.");
+    }
+  } catch (err) {
+    res.render("index", {
+      data: {
+        username: req.body.username,
+      },
+      errors: {
+        common: {
+          msg: err.message,
+        },
+      },
+    });
+  }
+}
+
 module.exports = {
   getLogin,
+  login,
 };
